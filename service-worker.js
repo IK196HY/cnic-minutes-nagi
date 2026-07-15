375e41a2103f2d7611506a9deadcdddcd9157c28
IK196HY <kkc196ns@gmail.com>
2026-07-16 03:09:13 +0900

    Refresh PWA cache for transcription fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nagi-minutes-v1";
+const CACHE_NAME = "nagi-minutes-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.json"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
